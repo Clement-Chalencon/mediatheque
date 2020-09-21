c73e3018d35675f983d6316042e143fb1536a541
Clement-Chalencon <clement.chalencon@le-campus-numerique.fr>
2020-09-21 11:11:09 +0200

    initial commit

---
+++ database/migrations/2020_09_21_085405_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price');
+            $table->decimal('weight');
+            $table->boolean('active')->default(false);
+            $table->integer('quantity')->defaut(0);
+            $table->integer('quantity_alert')->default(10);
+            $table->string('image')->nullable();
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
